1ac7d7a65c14ea8a558b2e995b26c17c426fb09d
Eduado-Malhao <eduardo.malhao@outlook.com>
2022-10-24 16:24:38 -0300

    exercicios de JS

---
+++ fundamentos/javascript/exe.js
@@ -0,0 +1,37 @@
+// const currentHour = 22
+// let message = ""
+
+// if (currentHour === 22) {
+//   console.log(message = 'Não deveríamos comer nada, é hora de dormir');
+// }
+
+// else if (currentHour >= 18 && currentHour <= 22) {
+//   console.log(message = 'Rango da noite, vamos jantar :D');
+// }
+
+// else if (currentHour == 14 && currentHour <= 18) {
+//   console.log(message = 'nhaaa')
+// } 
+
+//exe 02
+
+// let weekDAy = 'nhaa'
+// // let weekDAys = ['seg' , 'ter' , 'quar' , 'quin' , 'sex']
+
+// if (weekDAy === 'segunda' || weekDAy === 'terca' || weekDAy === 'quarta' || weekDAy === 'quinta' || weekDAy === 'sexta') {
+//   console.log('trybe')
+// }
+
+// else {
+//   console.log('fds')
+// }
+
+// if (weekDAys.includes(weekDAy)){
+//   console.log('trybe');
+// }
+// else {
+//   console.log('fds');
+// }
+
+//exe03
+
