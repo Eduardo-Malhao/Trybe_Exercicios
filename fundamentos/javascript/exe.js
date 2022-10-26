ae66b80956eb68aa0299294391ab51a9a4dd1904
Eduado-Malhao <eduardo.malhao@outlook.com>
2022-10-25 22:26:34 -0300

    JS_exercises

--- fundamentos/javascript/exe.js
+++ fundamentos/javascript/exe.js
@@ -40,7 +40,69 @@
 //   console.log(sequence[i]);
 // }
 
-let num = 0;
-for(let index = 0; index <= 100; index+=1 ){
-    console.log(num += index);
+// let num = 0;
+// for(let index = 0; index <= 100; index+=1 ){
+//     console.log(num += index);
+// }
+
+// exe 04
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+//     console.log('ola ' + info.personagem);
+  
+//     info['recorrente'] = 'sim';
+//     console.log(info);
+//     for(let objeto in info) {
+//         console.log(objeto);
+//         console.log(info[objeto]);
+// }
+
+// // exe05
+// let leitor = {
+//     nome: 'Julia',
+//     sobrenome: 'Pessoa',
+//     idade: 21,
+//     livrosFavoritos: [
+//       {
+//         titulo: 'O Pior Dia de Todos',
+//         autor: 'Daniela Kopsch',
+//         editora: 'Tordesilhas',
+//       },
+//     ],
+//   };
+//   leitor['fullName'] = leitor.nome + " " + leitor.sobrenome;
+
+//     console.log(leitor.fullName + " livro nhaa " + leitor.livrosFavoritos[0].titulo);
+
+//     leitor.livrosFavoritos[1] =
+//         {
+//             titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//             autor: 'JK Rowling',
+//             editora: 'Rocco',
+//         }
+
+// console.log("julia tem " + leitor.livrosFavoritos.length + " livros favoritos");
+
+// exe06
+
+let verificaPalindrome = ["xablau"];
+console.log(verificaPalindrome);
+
+function spliter (verificaPalindrome) {
+    return verificaPalindrome.split(),
+    console.log(verificaPalindrome);
 }
+function reverser (verificaPalindrome){
+    return verificaPalindrome.reverse(),
+    console.log(verificaPalindrome);
+}
+function joiner (verificaPalindrome) {
+    return verificaPalindrome.join(),
+    console.log(verificaPalindrome);
+}
+ 
+console.log(verificaPalindrome);
